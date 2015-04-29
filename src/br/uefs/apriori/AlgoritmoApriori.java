fce5c1d216497bf49c23f1ba406169c2820979f3
Tayane Cerqueira <engcomp.tayane@gmail.com>
2015-04-28 21:33:58 -0300

    gerando itemsets frequentes de tamanho maior que 1. falta fazer a geração de itemsets maiores que 2 e a geração das regras

--- src/br/uefs/apriori/AlgoritmoApriori.java
+++ src/br/uefs/apriori/AlgoritmoApriori.java
@@ -15,7 +15,7 @@ public class AlgoritmoApriori {
 	private double confianca = 0.5;
 	String arquivoTransacoes = "transacoes.txt";
 	private int quantTransacoes;
-	private ArrayList<String> items; //guarda todos os itens de todas as transações, incluindo as repetições
+	private ArrayList<String> transacoes; //guarda as transações. cada item do array é uma linda da transação. para pegar os itens separadamente é necessário quebrar a string
 	private HashMap<Integer, ArrayList<String>> itemset = new HashMap<Integer, ArrayList<String>>(); //conjuntos possíveis
 	private HashMap<Integer, ArrayList<String>> frequentItemset = new HashMap<Integer, ArrayList<String>>(); //conjuntos com o suporte mínimo
 	
@@ -38,6 +38,8 @@ public class AlgoritmoApriori {
 		geraItemsetFrequente(this.itemset.get(1), 1); //Gera o conjunto L1
 		geraItemset(this.frequentItemset.get(1), 1); //Gera o conjunto C2
 		geraItemsetFrequente(this.itemset.get(2), 2); //Gera o conjunto L2
+		geraItemset(this.frequentItemset.get(2), 2); //Gera o conjunto C3
+
 	}
 	
 	//Gera o conjunto C1 a partir do arquivo .txt
@@ -47,25 +49,27 @@ public class AlgoritmoApriori {
 		try {
 			BufferedReader dados = new BufferedReader(new FileReader(arquivoTransacoes));
 			List<String> itensTransacao = new ArrayList<String>();
-			this.items = new ArrayList<String>();
+			this.transacoes = new ArrayList<String>();
 			while (dados.ready()) { 
 				String linha = dados.readLine();
 				
 				if (linha.matches("\\s*")){
 					continue; 
 				}
-				
+		
+				this.transacoes.add(linha);
 				quantTransacoes++;
 				StringTokenizer token = new StringTokenizer(linha, " ");
 
 				while (token.hasMoreTokens()) {
 					
 					String item = new String(token.nextToken());
-					this.items.add(item);
+					//this.items.add(item);
 					
 					if(!itensTransacao.contains(item)){
 						itensTransacao.add(item);
-						System.out.println("C1: " + item);
+						//System.out.println("C1: " + item);
+						System.out.println("{" + item + "}");
 					}
 					
 				}
@@ -111,7 +115,8 @@ public class AlgoritmoApriori {
 					if(!aux1.equals(aux2)){
 						String conj = aux1.concat(" " + aux2);
 						lista.add(conj);
-						System.out.println("C" + (nivel+1) + ": " + conj);
+						//System.out.println("C" + (nivel+1) + ": " + conj);
+						System.out.println("{" + conj + "}");
 						
 					}
 					
@@ -122,7 +127,13 @@ public class AlgoritmoApriori {
 			this.itemset.put(2, lista);
 			
 		}
+		//Geração dos conjuntos de tamanho maior que 1
 		else{
+			
+			/*
+			 * LUCAS! É AQUI QUE FALTA FAZER A PARTE DE COMBINAÇÃO MAIOR QUE 1!! ESTÁ OBVIAMENTE INCOMPLETO!
+			 * SE COMPILAR VAI DAR ERRO! ;)
+			 */
 			
 			ArrayList<String> lista = new ArrayList<String>();
 			
@@ -137,18 +148,6 @@ public class AlgoritmoApriori {
 					System.out.println(str1);
 					
 				}
-				
-				
-				/*for(int j = i+1; j < frequentItemset.size(); j++){
-					String aux2 = frequentItemset.get(j);
-					if(!aux1.equals(aux2)){
-						String conj = aux1.concat(" " + aux2);
-						lista.add(conj);
-						System.out.println("C2: " + conj);
-						
-					}
-					
-				}*/
 				
 			}
 			
@@ -166,50 +165,93 @@ public class AlgoritmoApriori {
 		
 		if(nivel == 1){
 			
-			ArrayList<String> conjunto = new ArrayList<String>();
+			ArrayList<String> conjuntoFrequente = new ArrayList<String>();
 			
 			for (String item : itemset) {//Cada item do array é um conjunto de items
 				
 				int cont = 0;
-				for(int i = 0; i < items.size(); i++){
+				for (String transacao : this.transacoes) {//Varre o array de transações - cada posição do array é uma linha
 					
-					if(item.equals(items.get(i))){
-						cont++;
+					StringTokenizer token = new StringTokenizer(transacao, " "); //cada token é um item
+
+					while (token.hasMoreTokens()) {//Enquanto houver items na transação...
+						String t = new String(token.nextToken());
+						if(item.equals(t)){						
+							cont++;
+							break;
+						}
 					}
 					
 				}
 				
 				double suporteEncontrado = (double)cont/(double)quantTransacoes;
 				if(suporteEncontrado >= suporte){
-					System.out.println("O item: '" + item + "' possui o suporte necessário");
-					conjunto.add(item);
+					//System.out.println("O item: '" + item + "' possui o suporte necessário");
+					System.out.println("{" + item + "}");
+					conjuntoFrequente.add(item);
 				}
 				
 			}
 			
-			this.frequentItemset.put(1, conjunto);
+			this.frequentItemset.put(1, conjuntoFrequente);
 			
 		}
 		else{
 			
-			ArrayList<String> conjunto = new ArrayList<String>();
-			ArrayList<String> lista = new ArrayList<String>();
+			ArrayList<String> conjuntoFrequente = new ArrayList<String>();
 			
 			for (int i = 0; i < itemset.size(); i++) {
-				System.out.println(" - Conjunto " + i);
+				
+				ArrayList<String> conjunto = new ArrayList<String>();
 				String conj = itemset.get(i); //É necessário agora quebrar essa string em itens
+				//System.out.println("- Conjunto {" + conj + "}");
 				StringTokenizer token1 = new StringTokenizer(conj, " ");
 
 				while (token1.hasMoreTokens()) {
-					String str = new String(token1.nextToken());
-					conjunto.add(str);
-					System.out.println(str);	
+					String str1 = new String(token1.nextToken());
+					conjunto.add(str1);
 				}
 				
-				//Verifica se o conjunto é frequente
-				//FAZERR!!
+				int contTrans = 0; //Contador que irá contar em quantas transições o conjunto aparece
+				
+				//Verifica em quantas transações os itens do conjunto aparecem juntos
+				for (String transacao : this.transacoes) {//...verifico se ele está na transação
+					
+					//System.out.println("Transação: {" + transacao + "}");
+					StringTokenizer token2 = new StringTokenizer(transacao, " ");
+					int cont = 0;
+					while (token2.hasMoreTokens()) {
+						String itemTrans = new String(token2.nextToken()); //pego o item da transacao...
+						
+						//System.out.println("Varrendo conjunto...");
+						
+						for (String itemConjunto : conjunto) {//Pego um item do conjunto...
+							//System.out.println("Item do conjunto: " + itemConjunto);
+							if(itemTrans.equals(itemConjunto)){//Se o item da transacao é igual a um dos itens do conjunto...
+								//System.out.println(itemTrans + " == " + itemConjunto);
+								cont++;
+								break;
+							}
+						}
+
+					}
+					if(cont == nivel){
+						//System.out.println(" * Conjunto encontrado na transação {" + transacao + "}");
+						contTrans++;
+					}
+
+				}
+				
+				double suporteEncontrado = (double)contTrans/(double)quantTransacoes;
+				if(suporteEncontrado >= suporte){
+					//System.out.println("O conjunto {" + conj + "} possui o suporte necessário");
+					System.out.println("{" + conj + "}");
+					conjuntoFrequente.add(conj);
+				}
 				
 			}
+			
+			this.frequentItemset.put(1, conjuntoFrequente);
 			
 		}
 		
